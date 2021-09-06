ce4f7c0794c8e346574a72d369ff25c38a4495e1
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2021-09-06 16:22:39 -0300

    Bootstrap

--- front-and/src/App.tsx
+++ front-and/src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1>Olá Mundo</h1>
+      <h1 className="text-primary">Olá Mundo</h1>
     </div>
   );
 }
--- front-and/src/index.tsx
+++ front-and/src/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'assets/css/styles.css';
+
 
 import App from './App';
 
