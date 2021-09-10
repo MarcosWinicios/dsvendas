6981b05b61341998a927e0ac693e2aaa6570358c
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2021-09-10 16:12:21 -0300

    Datatable integration

--- front-and/src/types/sale.ts
+++ front-and/src/types/sale.ts
@@ -1,3 +1,27 @@
+import { type } from "os"
+import { Seller } from "./seller"
+
+export type Sale = {
+	id: number;
+	visited: number;
+	deals: number;
+	amount: number;
+	date: string;
+	seller: Seller;
+}
+
+export type SalePage = {
+	content?: Sale[];
+	last: boolean;
+	totalElements: number;
+	totalPages: number;
+	size?: number;
+	number: number;
+	first: boolean;
+	numberOfElements?: number;
+	empty?: boolean;
+}
+
 export type SalesSum = {
 	sellerName: string;
 	sum: number;
